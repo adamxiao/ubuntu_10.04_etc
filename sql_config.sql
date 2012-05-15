75eb8859f2a8a23465bb1d227f868d66480f6047
Adam Xiao <iefcuxy@gmail.com>
2012-05-15 15:24:48 +0800

    add oracle config file

---
+++ sql_config.sql
@@ -0,0 +1,16 @@
+set line 32767
+set heading off 
+set feedback off 
+set echo off 
+SET NEWPAGE none
+SET TRIMSPOOL off 
+SET SQLBLANKLINES off 
+SET TRIMOUT on
+SET TRIMS on
+set timing off 
+SET VER OFF
+
+alter session set nls_date_format='yyyy-mm-dd hh24:mi:ss'
+COLUMN MYSQL FORMAT A32767
+set array 5000
+
